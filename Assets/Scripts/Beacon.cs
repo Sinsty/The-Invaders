8190e1b0dd55641d58ab8dda0beb6b8f581a18f9
Sinsty <sevatop40@gmail.com>
2024-05-16 19:55:53 +0300

    Healthbar added and Sound Update

--- Assets/Scripts/Beacon.cs
+++ Assets/Scripts/Beacon.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering;
 
 public class Beacon : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class Beacon : MonoBehaviour
     [SerializeField] private float _turningOffSpeed = 1;
     [SerializeField] private GameObject _light;
     [SerializeField] private BeaconRotation _rotatingPanels;
+    [Header("Sounds")]
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _turningOffSound;
 
     public bool IsUsed { get; private set; } = false;
 
@@ -15,31 +19,39 @@ public class Beacon : MonoBehaviour
     {
         if (IsUsed)
         {
-            TurnOff();
+            TurningOffProcessUpdate();
         }
     }
 
-    private void TurnOff()
+    private void TurningOffProcessUpdate()
     {
-        if (_light.transform.localScale.magnitude >= 0.05 || _rotatingPanels.RotationSpeed >= 0.05f)
+        if (_light.transform.localScale.magnitude >= 0.05 || _rotatingPanels.RotationSpeed >= 0.05f || _audioSource.volume >= 0.001f)
         {
-            _light.transform.localScale = Vector3.Lerp(_light.transform.localScale, Vector3.up * 0.01f, _turningOffSpeed * Time.deltaTime);
+            _light.transform.localScale = Vector3.Lerp(_light.transform.localScale, Vector3.up * 0.0001f, _turningOffSpeed * Time.deltaTime);
             _rotatingPanels.RotationSpeed = Mathf.Lerp(_rotatingPanels.RotationSpeed, 0.01f, _turningOffSpeed * Time.deltaTime);
+            _audioSource.volume = Mathf.Lerp(_rotatingPanels.RotationSpeed, 0.01f, _turningOffSpeed * Time.deltaTime);
         }
         else if (_light.activeInHierarchy)
         {
             _light.SetActive(false);
         }
-        else
+        else if (_rotatingPanels.RotationSpeed != 0)
         {
             _rotatingPanels.RotationSpeed = 0;
+        }
+        else if (_audioSource.volume != 0)
+        {
+            _audioSource.volume = 0;
         }
     }
 
     public void Use()
     {
-        print("Beacon Used");
-        IsUsed = true;
-        OnUsed.Invoke();
+        if (IsUsed == false)
+        {
+            IsUsed = true;
+            OnUsed.Invoke();
+            _audioSource.PlayOneShot(_turningOffSound);
+        }
     }
 }
--- Assets/Scripts/UseBeaconButton.cs
+++ Assets/Scripts/UseBeaconButton.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class UseBeaconButton : MonoBehaviour, IInteractable
 {
     [SerializeField] private Beacon _beacon;
+    [SerializeField] private Animator _buttonAnimator;
+    [SerializeField] private GameObject _UIButtonVisual;
 
     public void Interact()
     {
-        print("ButtonPressed");
+        _buttonAnimator.SetTrigger("Use");
+        _UIButtonVisual.SetActive(false);
         _beacon.Use();
     }
 }
