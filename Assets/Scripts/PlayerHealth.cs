e7a31210c5de0ddf9b3fe0b0772107f868b4506e
Sinsty <sevatop40@gmail.com>
2024-05-18 15:38:50 +0300

    Camps enviroment added

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,19 +3,29 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float Health { get; private set; }
+    public float _health;
+    public float Health
+    {
+        get { return _health; }
+        private set { _health = SetHealth(value); }
+    }
+
 
 
     [SerializeField] private float _maxHealth = 100;
-    [SerializeField] private Image _hpBar;
+    [SerializeField] private float _timeToStartHealing = 10;
+    [SerializeField] private float _healingSpeed;
     [Header("Damage Effects")]
     [SerializeField] private AudioClip _damageTakeAudio;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private Animator _damageTakeAnimator;
     [Header("UI")]
+    [SerializeField] private Image _hpBar;
     [SerializeField] private GameObject gameplayUI;
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private GameObject GamePause;
+
+    private float _timerToHealing = 10;
 
     private float _hpBarFillAmmount;
 
@@ -27,13 +37,28 @@ public class PlayerHealth : MonoBehaviour
 
     private void Update()
     {
+        if (Time.time > _timerToHealing)
+        {
+            Heal(_healingSpeed * Time.deltaTime);
+        }
+
         _hpBar.fillAmount = Mathf.Lerp(_hpBar.fillAmount, _hpBarFillAmmount, Time.deltaTime * 5);
+    }
+
+    public void Heal(float health)
+    {
+        health = Mathf.Abs(health);
+        Health += health;
+
+        UpdateHealthBar();
     }
 
     public void DealDamage(float damage)
     {
         damage = Mathf.Abs(damage);
         Health -= damage;
+
+        _timerToHealing = Time.time + _timeToStartHealing;
 
         _audioSource.pitch = Random.Range(0.8f, 1.2f);
         _audioSource.PlayOneShot(_damageTakeAudio);
@@ -63,5 +88,11 @@ public class PlayerHealth : MonoBehaviour
     private void UpdateHealthBar()
     {
         _hpBarFillAmmount = Health / _maxHealth;
+    }
+
+    private float SetHealth(float value)
+    {
+        value = Mathf.Clamp(value, 0, _maxHealth);
+        return value;
     }
 }
