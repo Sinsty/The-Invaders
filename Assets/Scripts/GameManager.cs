9b293b7142d5911911cdb90d9621fc23084117a7
Mirafy <161581457+Mirafy@users.noreply.github.com>
2024-05-23 00:43:10 +0500

    History and teleport done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     public void Quit(){
         Application.Quit();        
     }
     
     public void StartGame(){
+        SceneManager.LoadScene("History");
+    }
+
+    public void Skip(){
         SceneManager.LoadScene("maket");
     }
 
